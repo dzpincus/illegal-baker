0aafe55314f13ec252fa3d282493d2d5128c9d99
Dylan Pincus <dpincus95@gmail.com>
2022-06-21 22:59:03 -0400

    Revert to server

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: 'server',
   // mode: 'spa',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
