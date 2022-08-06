2a45211eab93e18b2732b85a025e539a049aa528
Dylan Pincus <dpincus95@gmail.com>
2022-08-06 12:18:06 -0400

    add debugger

--- components/Checkout/Delivery.vue
+++ components/Checkout/Delivery.vue
@@ -196,6 +196,7 @@ export default {
                         }
                     }
                     self.deliveryValid = valid;
+                    debugger;
                 }
             );
         },
