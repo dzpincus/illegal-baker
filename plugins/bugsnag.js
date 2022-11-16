e8305ff1acd1260718dc59cb028cddc26e9ca2ed
Dylan Pincus <dpincus95@gmail.com>
2022-11-16 01:46:48 -0500

    key

--- plugins/bugsnag.js
+++ plugins/bugsnag.js
@@ -5,7 +5,7 @@ import bugsnag from '@bugsnag/js'
 import BugsnagVue from '@bugsnag/plugin-vue'
 
 const bugsnagClient = bugsnag.start({
-  apiKey: process.env.bugsnagKey,
+  apiKey: '3eee35450df45673a45bad3cd7e748c3',
   plugins: [new BugsnagVue()], // this is important
 })
 
